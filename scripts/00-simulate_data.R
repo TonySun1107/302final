d6288e72b60db6471b7f0ed25418035c2a87d11e
Wentao Sun <156039016+TonySun1107@users.noreply.github.com>
2024-04-12 17:19:23 -0400

    simulated

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,25 +1,35 @@
 #### Preamble ####
-# Purpose: Simulates an analysis dataset about Pokemon level and weakness against fire
-# Author: Rohan Alexander
-# Date: 26 March 2024
-# Contact: rohan.alexander@utoronto.ca
-# License: MIT
-# Pre-requisites: -
-# Any other information needed? -
-
+# Purpose: Simulates the desired data.
+# Author: Wentao Sun
+# Data: 28 March 2024
+# Contact: went.sun@mail.utoronto.ca
 
 #### Workspace setup ####
-library(tidyverse)
+
+#install.packages("dplyr")
+#install.packages("ggplot2")
+
+library(dplyr)
+library(ggplot2)
+
 
 #### Simulate data ####
-data <- 
-  tibble(
-    level = rnorm(n = 1000, mean = 100, sd = 10) |> floor(),
-    weakness = sample(x = c("fire", "not fire"), size = 1000, replace = TRUE)
-  )
+set.seed(300) 
 
+n <- 200
 
+simulated_data <- tibble(
+  water_id <- rep(1:50, each = 4),
+  #Generate pH data, assuming pH follows a normal distribution
+  pH_value <- rnorm(n, mean=7.5, sd=0.5),
+  #Generate dissolved oxygen data, assuming that dissolved oxygen follows a normal distribution
+  dissolved_oxygen <- rnorm(n, mean=8, sd=2),
+  #Generate air temperature data, assuming temperatures follow a normal distribution
+  air_temp <- rnorm(n, mean=20, sd=5),
+  #Generate water temperature data, assuming temperatures follow a normal distribution
+  water_temp <- rnorm(n, mean=20, sd=5),
+  #Generate salinity data, assuming temperatures follow a normal distribution
+  Salinity <- rnorm(n, mean=1, sd=0.5)
+)
 
-
-
-
+head(simulated_data)
