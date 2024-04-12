8e2d60338f7e034da9ae5a0a369748c4a3e30cf5
Wentao Sun <156039016+TonySun1107@users.noreply.github.com>
2024-04-12 17:21:37 -0400

    download datasets

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,26 +1,21 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
-
+# Purpose: Downloads and saves the data from Data.gov(U.S. Government Data Platform)
+# Author: Wentao Sun
+# Data: 29 March 2024
+# Contact: went.sun@mail.utoronto.ca
 
 #### Workspace setup ####
-library(opendatatoronto)
+
 library(tidyverse)
-# [...UPDATE THIS...]
+library(readr)
 
 #### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
+# read in the datasets
+# full child by word (in American Sign Language) raw data
+raw_waterquality_data <- 
+  read_csv(
+    file = "https://ecos.fws.gov/ServCat/Reference/Profile/117348"
+    )
 
 #### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
+write_csv(raw_waterquality_data, "data/raw_data/Raw_Data_WaterQuality.csv") 
