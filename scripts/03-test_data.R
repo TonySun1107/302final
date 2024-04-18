af77a6c3f044e51c8687e24e0b9c215e2ca62327
Wentao Sun <156039016+TonySun1107@users.noreply.github.com>
2024-04-18 09:18:40 -0400

    Update 03-test_data.R

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,32 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Test simulated data.
+# Author: Wentao Sun
+# Data: 2 April 2024
+# Contact: went.sun@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Create simulated data.
+# Any other information needed? None.
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
 
 #### Test data ####
+# test 1
+simulated_data$Read_Date |>
+  class() == "character"
+
+# test 2
+simulated_data$Site_Id |>
+  max() == 50
+
+# test 3
+simulated_data$Year |>
+  class() == "integer"
+
+# test 4
+# variable
+num_uniq <-
+  simulated_vocab_data$production |>
+  unique()
+length(num_uniq) == 2
