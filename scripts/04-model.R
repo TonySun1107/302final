e18b70400280a0222e9a558cf784039927a22a63
Wentao Sun <156039016+TonySun1107@users.noreply.github.com>
2024-04-18 09:21:43 -0400

    Update 04-model.R

--- scripts/04-model.R
+++ scripts/04-model.R
@@ -1,36 +1,37 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Model.
+# Author: Wentao Sun
+# Data:4 April 2024
+# Contact: went.sun@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Create simulated data.
+# Any other information needed? None.
+
+set.seed(6607)
+
+# Sample 80% of the data indices for training
+train_samp <- sample(seq_len(nrow(water_cleaneddata)), size = floor(0.8 * nrow(water_cleaneddata)))
+
+# Split data into training and testing sets
+train <- water_cleaneddata[train_samp, ]
 
 
-#### Workspace setup ####
-library(tidyverse)
-library(rstanarm)
+train <-water_cleaneddata %>% 
+  mutate(year_1999 = ifelse(as.numeric(as.character(year)) >= 1999,1,0)) %>% 
+  select(!c(Air.Temp,Water.Depth,year))
 
-#### Read data ####
-analysis_data <- read_csv("data/analysis_data/analysis_data.csv")
 
-### Model data ####
-first_model <-
-  stan_glm(
-    formula = flying_time ~ length + width,
-    data = analysis_data,
-    family = gaussian(),
-    prior = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE),
-    prior_aux = exponential(rate = 1, autoscale = TRUE),
-    seed = 853
-  )
+# Run a linear model using the transformed year variable and other predictors
+lin_model <- lm(Oxygen ~ Salinity + pH + Depth + Water.Temp + year_1999,
+                data = train)
 
+# Diagnostic plots for the linear model
+par(mfrow = c(2, 2))
+plot(lin_model)
 
 #### Save model ####
 saveRDS(
-  first_model,
+  lin_model,
   file = "models/first_model.rds"
 )
 
